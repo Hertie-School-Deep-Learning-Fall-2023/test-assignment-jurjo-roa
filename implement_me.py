f79d28e49527ad2cb8a0451240efda9964ca79f9
Jorge Roa <64996320+jurjoroa@users.noreply.github.com>
2023-09-11 09:29:57 +0200

    Update implement_me.py

--- implement_me.py
+++ implement_me.py
@@ -2,4 +2,20 @@
 def lcm(a, b):
   """compute Least Common Multiple between two integers."""
 
+  # choose the greater number
+  if a > b:
+    greater = a
+  else:
+    greater = b
+
+  while(True):
+    if((greater % a == 0) and (greater % b == 0)):
+      lcm = greater
+      break
+    greater += 1
+
+  return lcm
   pass
+
+
+lcm(4, 99)
